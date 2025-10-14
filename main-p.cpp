7943ecf27aeef3d93c0ed7aac939a908cf1da948
Rubens Montanha <rubens.montanha@edu.pucrs.br>
2025-10-14 18:49:45 -0300

    Update main-p.cpp

--- main-p.cpp
+++ main-p.cpp
@@ -9,7 +9,7 @@
 
 #define SIMULATION_STEPS 100
 #define DELTA_TIME 0.1f
-#define NUM_SIM 31
+#define NUM_SIM 62
 
 int main(int argc, char** argv) {
 
@@ -32,24 +32,35 @@ int main(int argc, char** argv) {
 
     if (my_rank == 0) {
         // Coordenador
+        float sim_time = 0.0f;
+
+        // std::cout << "Coordenador Iniciando" << std::endl;
 
         auto start = std::chrono::high_resolution_clock::now();
 
         for (int i = 1; i < proc_n; i++) {
             MPI_Send(&sim_seed, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
+            // std::cout << my_rank << ">> Enviei a seed " << sim_seed << " para o " << i << "/" << proc_n << std::endl;
+
 
             sim_seed++;
         }
 
         while (sim_recv < NUM_SIM) {
-            MPI_Recv(&sim_seed, 1, MPI_INT, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
+            float payload = 0.0f;
+
+            MPI_Recv(&payload, 1, MPI_FLOAT, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
+
+            sim_time += payload;
 
             sim_recv++;
 
             if (sim_seed > NUM_SIM) {
                 MPI_Send(&invalid, 1, MPI_INT, status.MPI_SOURCE, tag, MPI_COMM_WORLD);
+                // std::cout << my_rank << ">> Enviei a seed " << -1 << " para o " << status.MPI_SOURCE << std::endl;
             } else {
                 MPI_Send(&sim_seed, 1, MPI_INT, status.MPI_SOURCE, tag, MPI_COMM_WORLD);
+                // std::cout << my_rank << ">> Enviei a seed " << sim_seed << " para o " << status.MPI_SOURCE << std::endl;
 
                 sim_seed++;
             }
@@ -58,13 +69,19 @@ int main(int argc, char** argv) {
         auto end = std::chrono::high_resolution_clock::now();
         
         std::chrono::duration<double> elapsed = end - start;
+        std::cout << "Simulation Time: " << (sim_time/NUM_SIM) << " seconds\n";
         std::cout << "Execution time: " << elapsed.count() << " seconds\n";
 
     } else {
         // Trabalhador
 
+        // std::cout << "Trabalhador " << my_rank << " Iniciando" << std::endl;
+
         while (sim_seed != -1) {
+            // std::cout << my_rank << ">> Aguardando uma seed" << std::endl;
+
             MPI_Recv(&sim_seed, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+            // std::cout << my_rank << ">> Recebi a seed " << sim_seed << std::endl;
 
             if (sim_seed == -1)
                 continue;
@@ -105,54 +122,4 @@ int main(int argc, char** argv) {
     MPI_Finalize();
     
     return 0;
-
-    // // Criar o mundo
-    // World* world = world_create(20.0f, 20.0f); // largura, altura, maxAgents
-    // if (!world) {
-    //     printf("Erro ao criar o mundo!\n");
-    //     return 1;
-    // }
-    //
-    // // 2. Cria as células
-    //
-    //
-    // // 3. Cria os marcadores de auxina (exemplo: 5 auxinas)
-    //
-    // export_cells_csv(world, "C:/Users/Rubens/CLionProjects/BioCrowdsCP/output/cells.csv");
-    // save_markers_csv(world, "C:/Users/Rubens/CLionProjects/BioCrowdsCP/output/markers.csv");
-    //
-    // return 0;
-    //
-    // // Inicializar posições aleatórias dos agentes
-    // for (int i = 0; i < world->maxAgents; i++) {
-    //     Agent* agent = world->agents[i];  // agents é um array de ponteiros
-    //     agent->position.x = (float)(rand() % (int)world->dimension.x);
-    //     agent->position.z = (float)(rand() % (int)world->dimension.z);
-    //     agent->position.y = 0.0f;
-    // }
-    //
-    // // Loop principal de simulação
-    // for (int step = 0; step < SIMULATION_STEPS; step++) {
-    //     printf("Step %d\n", step);
-    //     for (int i = 0; i < world->maxAgents; i++) {
-    //         Agent* agent = world->agents[i];
-    //
-    //         // Atualizar comportamento do agente
-    //         agent_find_near_auxins(agent);
-    //         agent_calculate_direction(agent);
-    //         agent_calculate_velocity(agent);
-    //         agent_update_position(agent, DELTA_TIME);
-    //
-    //         printf("Agent %d position: (%.2f, %.2f, %.2f)\n",
-    //                i,
-    //                agent->position.x,
-    //                agent->position.y,
-    //                agent->position.z);
-    //     }
-    // }
-    //
-    // // Liberar memória
-    // world_destroy(world);
-    //
-    // return 0;
 }
